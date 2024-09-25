eae2147ede608d0056b62a58c945ea17543352de
Drobilko Danila <ddrobilko@mail.ru>
2024-09-25 09:38:25 +0300

    Добавлены библиотеки

---
+++ template/scripter.php
@@ -0,0 +1,10 @@
+         <script src="../js/cdn_js/v0.js" async="async"></script>
+         <script src="../js/cdn_js/jquery.min.js" async="async" custom-element="amp-iframe"></script>
+         <script src="../js/cdn_js/jquery.maskedinput.min.js" async="async" custom-element="amp-lightbox"></script>
+         <script src="../js/cdn_js/amp-mustache-0.2.js" async="async" custom-element="amp-list"></script>
+         <script src="../js/cdn_js/amp-list-0.1.js" async="async" custom-template="amp-mustache"></script>
+         <script src="../js/cdn_js/amp-lightbox-0.1.js" async="async" custom-element="amp-bind"></script>
+         <script src="../js/cdn_js/amp-iframe-0.1.js" async="async" custom-element="amp-carousel"></script>
+         <script src="../js/cdn_js/amp-carousel-0.1.js" async="async" custom-element="amp-analytics"></script>
+         <script src="../js/cdn_js/amp-bind-0.1.js" type="text/javascript"></script>
+         <script src="../js/cdn_js/amp-analytics-0.1.js" type="text/javascript"></script>
