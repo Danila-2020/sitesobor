e2826bcba689d8a423d63eaff6dfcefc089f4f05
Drobilko Danila <ddrobilko@mail.ru>
2024-09-26 10:36:57 +0300

    Coffee CSS можно выбросить

--- template/christeninghead.php
+++ template/christeninghead.php
@@ -6,10 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <meta name="amp-google-client-id-api" content="googleanalytics">
-    <!--<link rel="stylesheet" href="css/style.css">-->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.css">
-    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../css/christenimg.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-grid.css">
+    <link rel="stylesheet" href="../css/bootstrap-reboot.css">
     <link rel="icon" type="image/png" href="/16x16/files/sobor-small-rounded.png" />
     <link rel="apple-touch-icon" href="/57x57/files/sobor-small-rounded.png"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/brands.css" integrity="sha384-i2PyM6FMpVnxjRPi0KW/xIS7hkeSznkllv+Hx/MtYDaHA5VcF0yL3KVlvzp8bWjQ" crossorigin="anonymous">
