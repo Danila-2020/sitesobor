514e80e09be6dbfb34789699c797b0de1c7ebb83
Drobilko Danila <ddrobilko@mail.ru>
2024-10-07 17:02:21 +0300

    Отремонтированно редактирование новости

--- updateunewsadminsubmit.php
+++ updateunewsadminsubmit.php
@@ -5,6 +5,8 @@ include('template/head.php');
 include('template/barber.php');
 
 $id = $_SESSION['id'];
+$idunews = $_SESSION['idunews'];
+
 if(empty($id)){
     echo('<script>window.location.href="index.php"</script>');
 }
@@ -13,8 +15,10 @@ if(isset($_POST['submit'])){
     $utitle = $_POST['utitle'];
     $udescription = $_POST['udescription'];
     $textunews = $_POST['textunews'];
-    $query = ("UPDATE `unews` SET `utitle`='$utitle',`udescription`='$udescription',`textunews`='$textunews',`statusunews`='active',`dateunews`='' WHERE `id_unews` = 1");
+    $dateunews = $_POST['dateunews'];
+    $query = ("UPDATE `unews` SET `utitle`='$utitle',`udescription`='$udescription',`textunews`='$textunews',`statusunews`='active',`dateunews`='$dateunews' WHERE `id_unews` = $idunews");
     $result = $mysqli->query($query);
+    //var_dump($query);
     echo("<script>alert('Изменения успешно внесенны!');</script>");
     echo('<script>window.location.href="updateunewsadmin.php"</script>');
 }
