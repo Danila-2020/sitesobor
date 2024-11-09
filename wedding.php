2f7f296ccfb1130d327b3be3ba73f157ed437cf4
Drobilko Danila <ddrobilko@mail.ru>
2024-11-09 15:27:55 +0300

    Подключен футер к Венчанию

--- wedding.php
+++ wedding.php
@@ -90,12 +90,6 @@ include('template/weddinghead.php');
         <p>Венчание — это не только формальность, но и глубокий духовный процесс, который требует осознания и уважения. Это начало новой жизни для супругов, основанной на любви, верности и взаимопонимании.</p>
     </div>
 </div>
-<footer>
-    <p><b><i>&copy; Колодочкин Алексей<br>
-    Дробилко Данила</i></b></p>
-</footer>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php
+include('template/weddingfooter.php');
+?>
