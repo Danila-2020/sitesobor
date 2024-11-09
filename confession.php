6712242aab8541315dfcdcc50aec27db5228d4e8
Drobilko Danila <ddrobilko@mail.ru>
2024-11-09 17:00:00 +0300

    Работа с таинством Исповедь

--- confession.php
+++ confession.php
@@ -1,178 +1,59 @@
 <?php
-session_start();
-require_once('bd.php');
-require_once('template/confessionhead.php');
-require_once('template/barber.php');
+include('template/confessionhead.php');
 ?>
-
-    <noscript><style amp-boilerplate="">body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-
-<script src="https://cdn.ampproject.org/v0.js" async="async"></script>
-<script src="https://cdn.ampproject.org/v0/amp-bind-0.1.js" async="async" custom-element="amp-bind"></script>
-<script src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js" async="async" custom-element="amp-analytics"></script></head>
-<body class="land-see ">
-    <amp-analytics type="metrika">
-        <script type="application/json">
-            {
-                "vars": {
-                    "counterId": "53592163"
-                }
-            }
-        </script>
-    </amp-analytics>
-
+<body>
+<?php
+include('template/sacramentsnav.php');
+?>
+<div class="container">
+    <h1 class="article-title">Таинство Исповеди</h1>
     
-<div class="site-article">
-    <div class="content-wrap"><!-- content-wrap -->
-        <a href="/" class="block relative sm-hide md-hide lg-hide logo-wrap logo-wrap-mob"></a>
-
-        <div class="max-width-4 mx-auto p2"><!-- full-width-wrap -->
-            <div class="border border-grey bg-white-a70 rounded clearfix p2"><!-- clearfix -->
-                
-<ul class="center h2 list-reset mt0 head-menu">
-    <li class="inline-block mr1">
-        <a href="scedule.php">Расписание богослужений</a>
-    </li>
-    <li class="inline-block mr1">
-        <a [class]="aboutItem" on="tap:AMP.setState({sacramentsItem: null, sacramentsMenu: null, activitiesItem: null, activitiesMenu: null, aboutItem: 'underline', aboutMenu: 'center h4 list-reset'})">О соборе</a>
-    </li>
-    <li class="inline-block mr1">
-        <a [class]="activitiesItem" on="tap:AMP.setState({aboutItem:null, aboutMenu: null, sacramentsItem: null, sacramentsMenu: null, activitiesItem: 'underline', activitiesMenu: 'center h4 list-reset'})">Деятельность</a>
-    </li>
-    <li class="inline-block mr1">
-        <a [class]="sacramentsItem" on="tap:AMP.setState({aboutItem:null, aboutMenu: null, activitiesItem: null, activitiesMenu: null, sacramentsItem: 'underline', sacramentsMenu: 'center h4 list-reset'})">Таинства</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="note.php">Подать записку</a>
-    </li>
-</ul>
-
-<ul class="center h4 list-reset hide" [class]="aboutMenu||'hide'">
-    <li class="inline-block mr1">
-        <a class="" href="clergy.php">Духовенство</a>
-    </li>
-    <li class="inline-block mr1">
-        <a class="" href="/site/article?id=1">История</a>
-    </li>
-    <li class="inline-block mr1">
-        <a class="" href="/site/article?id=5">Роспись</a>
-    </li>
-</ul>
-
-<ul class="hide" [class]="activitiesMenu||'hide'">
-    <li class="inline-block mr1">
-        <a href="#">Воскресная школа</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="#">Молодежный центр</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="#">Библиотека</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="#">Социальная деятельность</a>
-    </li>
-</ul>
-
-<ul class="center h4 list-reset hide" [class]="sacramentsMenu||'hide'">
-    <li class="inline-block mr1">
-        <a href="christening.php">Крещение</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="wedding.php">Венчание</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="#">Исповедь</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="eucharist.php">Причастие</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="unction.php">Соборование</a>
-    </li>
-</ul>
-
-<hr>
-
-        <div class="social">
-            <ul class="social-share">
-              <li><a href="#"><i class="fa fa-telegram"></i></a></li>
-              <li><a href="#"><i class="fa fa-vk"></i></a></li>
-              <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
-              <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-              <li><a href="#"><i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-
-
-            <ul class="list-reset breadcrumbs">
-                    <li class="inline-block mr1">
-                                    <a href="index.php">Главная</a>
-                            </li>
-                    <li class="inline-block mr1">
-                                    <a href="#">Таинства</a>
-                            </li>
-                    <li class="inline-block mr1">Исповедь</li>
-            </ul>
-                <!--Тут вставка цикла выборки из БД-->
-                <?php
-                $result = $mysqli->query("SELECT DISTINCT `id_sacraments`, `nsacraments`, `textsacraments`, `id_uprofile`, `images` 
-                FROM `sacraments` 
-                INNER JOIN `imgsacraments` 
-                ON `imgsacraments`.`id_sacramets` = `sacraments`.`id_sacraments` WHERE `nsacraments`='Исповедь'");
-                while($row = $result->fetch_array()){
-                    $img = base64_encode($row['images']);
-                    echo('<h1>'.$row['nsacraments'].'</h1>');
-                    ?>
-                    <img src="data:image/jpeg;base64, <?=$img?>" class="img-fluid" alt="image">
-                    <?php
-                    echo($row['textsacraments']);
-                };
-                ?>
-                </div>
-                <!--Конец цикла вывода-->
-            </div><!-- clearfix-end -->
-        </div><!-- full-width-wrap-end -->
-    </div><!-- content-wrap-end -->
+    <div class="article-content">
+        <p>Исповедь — это священное таинство в христианской традиции, которое позволяет верующим очищаться от грехов и восстанавливать свои отношения с Богом. Это важный шаг на пути к духовному возрождению и обновлению.</p>
+        
+        <img src="img/confession-1.jpeg" alt="Исповедь" class="img-fluid">
+        
+        <h2 class="section-title">История Исповеди</h2>
+        <p>Исповедь, как таинство, имеет свои корни в Библии. Иисус Христос дал Своим апостолам власть прощать грехи, что стало основой для практики исповеди в христианской церкви.</p>
+        
+        <img src="img/confession-2.jpeg" alt="Исторические обряды" class="img-fluid">
+        
+        <h2 class="section-title">Символика Исповеди</h2>
+        <p>Во время исповеди используются различные символы, такие как:</p>
+        <ul>
+            <li><strong>Слово Божие</strong> — основа для осознания грехов и покаяния.</li>
+            <li><strong>Священник</strong> — посредник между верующим и Богом, который исполняет роль духовного наставника.</li>
+            <li><strong>Молитва</strong> — средство обращения к Богу за прощением и милостью.</li>
+        </ul>
+        
+        <img src="img/confession-3.jpeg" alt="Молитва" class="img-fluid">
+        
+        <h2 class="section-title">Подготовка к Исповеди</h2>
+        <p>Подготовка к исповеди включает в себя:</p>
+        <ol>
+            <li>Самоанализ и осознание своих грехов.</li>
+            <li>Молитва о прощении и помощи в покаянии.</li>
+            <li>Выбор времени и места для исповеди.</li>
+            <li>Обсуждение с духовным лицом, если это необходимо.</li>
+        </ol>
+        
+        <img src="img/confession-4.jpeg" alt="Подготовка к исповеди" class="img-fluid">
+        
+        <h2 class="section-title">Процесс Исповеди</h2>
+        <p>Церемония исповеди обычно включает в себя следующие этапы:</p>
+        <ul>
+            <li>Вход в исповедальню или место для исповеди.</li>
+            <li>Признание своих грехов священнику.</li>
+            <li>Получение прощения и благословения.</li>
+            <li>Совершение покаянной молитвы и выполнение назначенной епитимьи.</li>
+        </ul>
+        
+        <img src="img/confession-5.jpg" alt="Процесс исповеди" class="img-fluid">
+        
+        <h2 class="section-title">Заключение</h2>
+        <p>Исповедь — это важный духовный процесс, который помогает верующим очищаться от грехов и укреплять свою веру. Это возможность для каждого обновить свои отношения с Богом и начать новый путь в жизни.</p>
+    </div>
 </div>
-
-
-
-<div class="bg-white alpha-90 fit relative pt1" style="height:fit-content;">
-
-<div class="social">
-            <ul class="social-share">
-              <li><a href="#"><i class="fa fa-telegram"></i></a></li>
-              <li><a href="#"><i class="fa fa-vk"></i></a></li>
-              <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
-              <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-              <li><a href="#"><i class="fa fa-skype"></i></a></li>
-            </ul>
-          </div>
-
-<ul class="mx-auto center h2 list-reset">
-    <li class="inline-block mr1">
-        <a href="contacts.php">Задать вопрос</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="addnote.php">Подать записку</a>
-    <li>
-    <li class="inline-block mr1">
-        <a href="#">Контакты</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="#">Новости собора</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="http://blago-kavkaz.ru/article/blog?catids%5B0%5D=1" target="_blank">Новости епархии</a>
-    </li>
-    <li class="inline-block mr1">
-        <a href="http://www.patriarchia.ru/db/news/" target="_blank">Общецерковные новости</a>
-    </li>
-</ul>
-       <div class="relative">
-            <amp-img class="" src="img/mountains-no-sky-sharpened.png" width="1600" height="254" layout="responsive"></amp-img>
-        </div>
-</div>
-</body>
-</html>
+<?php
+include('template/confessionfooter.php');
+?>
--- wedding.php
+++ wedding.php
@@ -2,44 +2,9 @@
 include('template/weddinghead.php');
 ?>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="index.php">Главная</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="scedule.php">Расписание богослужений</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    О соборе
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="clergy.php">Духовенство</a>
-                    <a class="dropdown-item" href="#">История</a>
-                    <a class="dropdown-item" href="#">Роспись</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Таинства
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="christening.php">Крещение</a>
-                    <a class="dropdown-item" href="wedding.php">Венчание</a>
-                    <a class="dropdown-item" href="confession.php">Исповедь</a>
-                    <a class="dropdown-item" href="eucharist.php">Причастие</a>
-                    <a class="dropdown-item" href="unction.php">Соборование</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Контакты</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<?php
+include('template/sacramentsnav.php');
+?>
 
 <div class="container">
     <h1 class="article-title">Таинство Венчания</h1>
