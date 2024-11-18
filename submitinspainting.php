2b3e187529bb183fe1b4d33af67b1b6d098b6c55
Drobilko Danila <ddrobilko@mail.ru>
2024-11-18 14:07:57 +0300

    Добавлен модуль inspainting для general

---
+++ submitinspainting.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+ob_start();
+
+require_once('bd.php');
+
+if(isset($_POST['submit'])){
+    $naimimgpainting = $_POST['naimimgpainting'];
+    $textimgpainting = $_POST['textimgpainting'];
+    $img_type = substr($_FILES['images'] ['type'], 0, 5);
+    $img_size = 15*1024*1024;
+
+    if(!empty($_FILES['images'] ['tmp_name']) && !empty($naimimgpainting) && !empty($textimgpainting)){
+        $images = addslashes(file_get_contents($_FILES['images'] ['tmp_name']));
+        $sel = ("SELECT MAX(id_painting) AS `id` FROM painting");
+        $result = $mysqli->query($sel);
+        while($row = $result->fetch_assoc()){
+            $id = $row['id'];
+        }
+        $id = intval($id);
+        var_dump($id);
+        $query = ("INSERT INTO `imgpainting`( `naimimgpainting`, `textimgpainting`, `images`, `id_painting`) 
+        VALUES ('$naimimgpainting','$textimgpainting','$images',$id)");
+        $result = $mysqli->query($query);
+        if($result){
+            header('Location: inspainting.php');
+            echo("<script>alert('Изменения произведены успешно')</script>");
+        }
+    }
+}
+?>
---
+++ template/paintinghead.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="css/barber.css">
+    <style>
+        .image-button {
+            background-color: transparent; /* Прозрачный фон */
+            border: none; /* Без рамки */
+            cursor: pointer; /* Курсор в виде руки при наведении */
+        }
+        .image-button img {
+            width: 50px; /* Ширина изображения */
+            height: auto; /* Автоматическая высота */
+        }
+    </style>
+
+    <title>Сайт Собор</title>
+</head>
